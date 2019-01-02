b70d8783630db6dd279ad4f897e0a8f2fa0de3d9
Liam Duffy <liamdanielduffy@gmail.com>
2019-01-02 18:18:22 -0500

    adds constants used by message body factory and message factory classes

---
+++ src/tes_client/constants/message_body_types.js
@@ -0,0 +1,17 @@
+export default {
+    HEARTBEAT: "heartbeat",
+    TEST: "test",
+    LOGON: "logon",
+    LOGOFF: "logoff",
+    PLACE_ORDER: "placeOrder",
+    REPLACE_ORDER: "replaceOrder",
+    CANCEL_ORDER: "cancelOrder",
+    GET_ORDER_STATUS: "getOrderStatus",
+    GET_ORDER_MASS_STATUS: "getOrderMassStatus",
+    GET_ACCOUNT_DATA: "getAccountData",
+    GET_ACCOUNT_BALANCES: "getAccountBalances",
+    GET_OPEN_POSITIONS: "getOpenPositions",
+    GET_WORKING_ORDERS: "getWorkingOrders",
+    GET_COMPLETED_ORDERS: "getCompletedOrders",
+    GET_EXCHANGE_PROPERTIES: "getExchangeProperties"
+}
