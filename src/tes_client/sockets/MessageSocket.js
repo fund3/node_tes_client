6ecca5be2c6e531b91fb5d9c045880fbb86ed893
Liam Duffy <liamdanielduffy@gmail.com>
2019-01-03 04:12:21 -0500

    renames method on MessageSocket to be more explicit about sending a serialized message, rather than a plain message

--- src/tes_client/sockets/MessageSocket.js
+++ src/tes_client/sockets/MessageSocket.js
@@ -1,15 +1,16 @@
 import * as zmq from "zeromq";
 
 class MessageSocket {
-    
-    constructor({ socket_endpoint }) {
-        this.socket = zmq.socket("dealer");
-        this.socket_endpoint = socket_endpoint
-    }
+	constructor({ socket_endpoint }) {
+		this.socket = zmq.socket("dealer");
+		this.socket_endpoint = socket_endpoint;
+	}
 
-    activate = () => this.socket.connect(this.socket_endpoint)
+	activate = () => {
+		this.socket.connect(this.socket_endpoint);
+	};
 
-    sendMessage = ({ message }) => this.socket.send(message)
+	sendSerializedMessage = ({ serialized_message }) => this.socket.send(serialized_message);
 }
 
 export default MessageSocket
