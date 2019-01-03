bfcf3c39e80820c3c5d8363994a214fb09557b96
Liam Duffy <liamdanielduffy@gmail.com>
2019-01-03 00:08:46 -0500

    adds class to encapsulate the functionality of the socket connected to the TES

---
+++ src/tes_client/sockets/TESSocket.js
@@ -0,0 +1,29 @@
+import * as zmq from "zeromq";
+
+class TESSocket {
+    
+    constructor({ curve_server_key, socket_endpoint }) {
+        this.socket_endpoint = socket_endpoint
+        this.socket = zmq.socket("dealer");
+        this.authenticate({ curve_server_key })
+        this.socket.on("close_zmq_sockets", () => this.socket.close());
+    }
+
+    authenticate = ({ curve_server_key }) => {
+        const curve_keypair = zmq.curveKeypair();
+        this.socket.curve_publickey = curve_keypair.public
+        this.socket.curve_secretkey = curve_keypair.secret
+        this.socket.curve_serverkey = curve_server_key;
+    }
+
+    activate = ({ onMessage }) => {
+        this.socket.connect(this.socket_endpoint)
+        this.socket.on('message', onMessage)
+    }   
+
+    sendMessage = ({ message }) => this.socket.send(message)
+
+    get = () => this.socket
+}
+
+export default TESSocket;
