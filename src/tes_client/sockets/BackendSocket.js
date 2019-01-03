c08dbec40bc3f19331c1d7c4112eab9499d0df8d
Liam Duffy <liamdanielduffy@gmail.com>
2019-01-03 00:23:41 -0500

    adds class to encapsulate socket connecting to backend

---
+++ src/tes_client/sockets/BackendSocket.js
@@ -0,0 +1,21 @@
+import * as zmq from "zeromq";
+
+class BackendSocket {
+
+    constructor({ tes_socket, socket_endpoint }) {
+        this.socket_endpoint = socket_endpoint
+        this.socket = zmq.socket("router");
+        this.socket.on("close_zmq_sockets", () => this.socket.close());
+        this.tes_socket = tes_socket
+    }
+
+    activate = () => {
+        this.socket.on('message', (_, message) => this.tes_socket.sendMessage({ message }))
+        this.socket.bindSync(this.socket_endpoint)
+    }
+
+    get = () => this.socket
+
+}
+
+export default BackendSocket;
