c134d9a470bd80c497da47df8ee05a6f9db6881e
Calvin Leung <calvinleungyk@gmail.com>
2019-01-05 00:07:09 +0800

    Added factory methods and refactor

--- src/tes_client/factories/MessageBodyFactory.js
+++ src/tes_client/factories/MessageBodyFactory.js
@@ -1,93 +1,113 @@
-import { message_body_types } from '~/tes_client/constants'
-import { AccountInfo } from '~/tes_client/common_types';
+import {AccountInfo} from '~/tes_client/common_types';
+import {message_body_types} from '~/tes_client/constants';
+import {PlaceOrderArguments} from '~/tes_client/order';
 
 class MessageBodyFactory {
-	static buildMessageBody = ({ message_body_type, message_body_contents }) => {
-		switch (message_body_type) {
-			case message_body_types.HEARTBEAT:
-				return { [message_body_types.HEARTBEAT]: "" };
 
-			case message_body_types.TEST:
-				return { [message_body_types.TEST]: message_body_contents };
+  static buildMessageBody = ({message_body_type, message_body_contents}) => {
+    switch (message_body_type) {
+      case message_body_types.HEARTBEAT:
+        return {[message_body_types.HEARTBEAT]: ""};
 
-			case message_body_types.LOGON_REQUEST:
-				return { [message_body_types.LOGON_REQUEST]: message_body_contents };
+	  case message_body_types.TEST:
+		return {[message_body_types.TEST]: message_body_contents};
 
-			case message_body_types.LOGOFF:
-				return { [message_body_types.LOGOFF]: "" };
+	  case message_body_types.LOGON_REQUEST:
+		return {[message_body_types.LOGON_REQUEST]: message_body_contents};
 
-			case message_body_types.PLACE_ORDER:
-				return { [message_body_types.PLACE_ORDER]: message_body_contents };
+	  case message_body_types.LOGOFF:
+		return {[message_body_types.LOGOFF]: ""};
 
-			case message_body_types.REPLACE_ORDER:
-				return { [message_body_types.REPLACE_ORDER]: message_body_contents };
+	  case message_body_types.PLACE_ORDER:
+		return {[message_body_types.PLACE_ORDER]: message_body_contents};
 
-			case message_body_types.REPLACE_ORDER:
-				return { [message_body_types.REPLACE_ORDER]: message_body_contents };
+	  case message_body_types.REPLACE_ORDER:
+		return {[message_body_types.REPLACE_ORDER]: message_body_contents};
 
-			case message_body_types.CANCEL_ORDER:
-				return { [message_body_types.CANCEL_ORDER]: message_body_contents };
+	  case message_body_types.CANCEL_ORDER:
+		return {[message_body_types.CANCEL_ORDER]: message_body_contents};
 
-			case message_body_types.GET_ORDER_STATUS:
-				return { [message_body_types.GET_ORDER_STATUS]: message_body_contents };
+	  case message_body_types.GET_ORDER_STATUS:
+		return {[message_body_types.GET_ORDER_STATUS]: message_body_contents};
 
-			case message_body_types.GET_ORDER_MASS_STATUS:
-				return { [message_body_types.GET_ORDER_MASS_STATUS]: message_body_contents };
+	  case message_body_types.GET_ORDER_MASS_STATUS:
+		return {[message_body_types.GET_ORDER_MASS_STATUS]:
+			message_body_contents};
 
-			case message_body_types.GET_ACCOUNT_DATA:
-				return { [message_body_types.GET_ACCOUNT_DATA]: message_body_contents };
+	  case message_body_types.GET_ACCOUNT_DATA:
+		return {[message_body_types.GET_ACCOUNT_DATA]: message_body_contents};
 
-			case message_body_types.GET_ACCOUNT_BALANCES:
-				return { [message_body_types.GET_ACCOUNT_BALANCES]: message_body_contents };
+	  case message_body_types.GET_ACCOUNT_BALANCES:
+		return {[message_body_types.GET_ACCOUNT_BALANCES]:
+			message_body_contents};
 
-			case message_body_types.GET_OPEN_POSITIONS:
-				return { [message_body_types.GET_OPEN_POSITIONS]: message_body_contents };
+	  case message_body_types.GET_OPEN_POSITIONS:
+		return {[message_body_types.GET_OPEN_POSITIONS]:
+			message_body_contents};
 
-			case message_body_types.GET_WORKING_ORDERS:
-				return { [message_body_types.GET_WORKING_ORDERS]: message_body_contents };
+	  case message_body_types.GET_WORKING_ORDERS:
+		return {[message_body_types.GET_WORKING_ORDERS]:
+			message_body_contents};
 
-			case message_body_types.GET_COMPLETED_ORDERS:
-				return { [message_body_types.GET_COMPLETED_ORDERS]: message_body_contents };
+	  case message_body_types.GET_COMPLETED_ORDERS:
+		return {[message_body_types.GET_COMPLETED_ORDERS]:
+			message_body_contents};
 
-			case message_body_types.GET_EXCHANGE_PROPERTIES:
-				return { [message_body_types.GET_EXCHANGE_PROPERTIES]: message_body_contents };
+	  case message_body_types.GET_EXCHANGE_PROPERTIES:
+		return {[message_body_types.GET_EXCHANGE_PROPERTIES]:
+			message_body_contents};
 
-			default:
-				return {};
-		}
-	};
+	  default:
+		return {};
+    }
+  };
 
-	static buildLogonMessageBody = ({ account_credentials }) => {
-		const message_body_type = message_body_types.LOGON_REQUEST;
-		const message_body_contents = { credentials: [account_credentials] };
-		const message_body = MessageBodyFactory.buildMessageBody({
-			message_body_type,
-			message_body_contents
-		});
-		return message_body;
-	};
+  static buildLogonMessageBody = ({account_credentials}) => {
+    const message_body_type = message_body_types.LOGON_REQUEST;
+	const message_body_contents = {credentials: [account_credentials]};
+	return MessageBodyFactory.buildMessageBody({
+	    message_body_type,
+	    message_body_contents
+	});
+  };
 
-	static buildHeartbeatMessageBody = () => {
-		const message_body_type = message_body_types.HEARTBEAT;
-		const message_body = MessageBodyFactory.buildMessageBody({ message_body_type });
-		return message_body;
-	};
+  static buildHeartbeatMessageBody = () => {
+    const message_body_type = message_body_types.HEARTBEAT;
+	return MessageBodyFactory.buildMessageBody(
+		{message_body_type});
+  };
 
-	static buildGetAccountBalancesMessageBody = ({ account_id }) => {
-        const message_body_type = message_body_types.GET_ACCOUNT_BALANCES
-        const message_body_contents = { accountInfo: new AccountInfo(account_id) }
-        const message_body = MessageBodyFactory.buildMessageBody({
-			message_body_type,
-			message_body_contents
-        });
-        return message_body
-	}
-	
-	static buildLogoffMessageBody = () => {
-		const message_body_type = message_body_types.LOGOFF
-		const message_body = MessageBodyFactory.buildMessageBody({ message_body_type })
-		return message_body
-	}
+  static buildGetAccountBalancesMessageBody = ({account_id}) => {
+    const message_body_type = message_body_types.GET_ACCOUNT_BALANCES;
+	const message_body_contents = {accountInfo: new AccountInfo(account_id)};
+	return MessageBodyFactory.buildMessageBody({
+	    message_body_type,
+		message_body_contents
+	});
+  };
+
+  static buildLogoffMessageBody = () => {
+    const message_body_type = message_body_types.LOGOFF;
+	return MessageBodyFactory.buildMessageBody(
+		{message_body_type});
+  };
+
+  static buildPlaceOrderMessageBody = ({placeOrderArguments}) => {
+    const message_body_type = message_body_types.PLACE_ORDER;
+    return MessageBodyFactory.buildMessageBody({
+		message_body_type,
+		placeOrderArguments
+	});
+  };
+
+  static buildGetOrderStatusMessageBody = ({accountInfo, orderId}) => {
+	const message_body_type = message_body_types.GET_ORDER_STATUS;
+	const message_body_contents = {accountInfo: accountInfo, orderID: orderId};
+	return MessageBodyFactory.buildMessageBody({
+	    message_body_type,
+		message_body_contents
+	});
+  };
 }
 
-export default MessageBodyFactory
+export default MessageBodyFactory;
--- src/tes_client/factories/MessageFactory.js
+++ src/tes_client/factories/MessageFactory.js
@@ -1,67 +1,75 @@
 import MessageBodyFactory from './MessageBodyFactory'
-import { message_types } from '~/tes_client/constants'
+import {message_types} from '~/tes_client/constants'
 
 class MessageFactory {
 
-    constructor({ 
-        account_id,
-        client_id, 
-        sender_comp_id,
-        account_credentials
-    }) {
-        this.account_id = account_id
-        this.client_id = client_id,
-        this.sender_comp_id = sender_comp_id,
-        this.account_credentials = account_credentials
+  constructor({
+    account_id,
+    client_id,
+    sender_comp_id,
+    account_credentials
+  }) {
+    this.account_id = account_id;
+    this.client_id = client_id;
+    this.sender_comp_id = sender_comp_id;
+    this.account_credentials = account_credentials;
+  }
+
+  buildMessage = ({message_body}) => ({
+    clientID: this.client_id,
+    senderCompID: this.sender_comp_id,
+    body: message_body
+  });
+
+  buildMessageContainer = ({message, message_type}) => {
+    switch(message_type) {
+      case message_types.REQUEST:
+        return {type: {[message_types.REQUEST]: message}};
+
+      case message_types.RESPONSE:
+        return {type: {[message_types.RESPONSE]: message}};
+
+      default: return {};
     }
+  };
 
-    buildMessage = ({ message_body }) => ({
-        clientID: this.client_id,
-        senderCompID: this.sender_comp_id,
-        body: message_body
-    })
+  buildRequestMessage = ({message_body}) => {
+    const message = this.buildMessage({message_body});
+    return this.buildMessageContainer({message,
+        message_type: message_types.REQUEST});
+  };
 
-    buildMessageContainer = ({ message, message_type }) => {
-        switch(message_type) {
-            case message_types.REQUEST:
-                return { type: { [message_types.REQUEST]: message } }
+  buildLogonMessage = () => {
+    const {account_credentials} = this;
+    const message_body = MessageBodyFactory.buildLogonMessageBody({account_credentials});
+    return this.buildRequestMessage({message_body});
+  };
 
-            case message_types.RESPONSE:
-                return { type: { [message_types.RESPONSE]: message } }
+  buildLogoffMessage = () => {
+    const message_body = MessageBodyFactory.buildLogoffMessageBody();
+    return this.buildRequestMessage({message_body});
+  };
 
-            default: return {}
-        }
-    }
+  buildHeartbeatMessage = () => {
+    const message_body = MessageBodyFactory.buildHeartbeatMessageBody();
+    return this.buildRequestMessage({message_body});
+  };
 
-    buildLogonMessage = () => {
-        const { account_credentials } = this
-        const message_body = MessageBodyFactory.buildLogonMessageBody({ account_credentials })
-        const message = this.buildMessage({ message_body })
-        const packaged_message = this.buildMessageContainer({ message, message_type: message_types.REQUEST })
-        return packaged_message
-    }
+  buildGetAccountBalancesMessage = () => {
+    const {account_id} = this;
+    const message_body = MessageBodyFactory.buildGetAccountBalancesMessageBody({account_id});
+    return this.buildRequestMessage({message_body});
+  };
 
-    buildLogoffMessage = () => {
-        const message_body = MessageBodyFactory.buildLogoffMessageBody()
-        const message = this.buildMessage({ message_body })
-        const packaged_message = this.buildMessageContainer({ message, message_type: message_types.REQUEST })
-        return packaged_message
-    }
+  buildPlaceOrderMessage = ({placeOrderArguments}) => {
+    const message_body = MessageBodyFactory.buildPlaceOrderMessageBody({placeOrderArguments});
+    return this.buildRequestMessage({message_body});
+  };
 
-    buildHeartbeatMessage = () => {
-        const message_body = MessageBodyFactory.buildHeartbeatMessageBody();
-        const message = this.buildMessage({ message_body })
-        const packaged_message = this.buildMessageContainer({ message, message_type: message_types.REQUEST })
-        return packaged_message
-    }
-
-    buildGetAccountBalancesMessage = () => {
-        const { account_id } = this
-        const message_body = MessageBodyFactory.buildGetAccountBalancesMessageBody({ account_id })
-        const message = this.buildMessage({ message_body })
-        const packaged_message = this.buildMessageContainer({ message, message_type: message_types.REQUEST })
-        return packaged_message
-    }
+  buildGetOrderStatusMessage = ({accountInfo, orderId}) => {
+    const message_body = MessageBodyFactory.buildGetOrderStatusMessageBody({accountInfo, orderId});
+    return this.buildRequestMessage({message_body});
+  };
 }
 
 export default MessageFactory;
