fbbc8daf2ef99bb90a08ed636b6a2089564172ff
Calvin Leung <calvinleungyk@gmail.com>
2019-01-04 23:48:26 +0800

    Added PlaceOrderArguments class

---
+++ src/tes_client/order/PlaceOrderArguments.js
@@ -0,0 +1,25 @@
+class PlaceOrderArguments {
+  constructor ({
+      accountInfo,
+      clientOrderId,
+      symbol,
+      side,
+      quantity,
+      orderType,
+      price,
+      timeInForce,
+      leverageType,
+      leverage,
+      clientOrderLinkId
+  }) {
+        this.accountInfo = accountInfo;
+        this.clientOrderId = clientOrderId;
+        this.symbol = symbol;
+        this.side = side;
+        this.quantity = quantity;
+        this.price = price;
+        // TODO: deal with optional arguments
+    }
+}
+
+export default PlaceOrderArguments;
