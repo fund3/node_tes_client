59d8508de28f03fcc5a2bc608cf3313f648243e0
Liam Duffy <liamdanielduffy@gmail.com>
2019-01-08 18:03:08 -0500

    modifies test to demonstrate setting account credentials for multiple accounts on a single client, and getting an account balance for a specific account

--- src/test.js
+++ src/test.js
@@ -4,29 +4,53 @@ require("dotenv").config();
 import uuidv4 from 'uuid/v4'
 
 import Client from './tes_client/Client'
-import AccountInfo from './tes_client/account/AccountInfo'
-import PlaceOrderArguments from './tes_client/order/PlaceOrderArguments'
+import AccountInfo from '~/tes_client/account/AccountInfo'
+import AccountCredentials from '~/tes_client/account/AccountCredentials'
 
-const account_id = parseInt(process.env.ACCOUNT_ID);
+const gemini_account_info = new AccountInfo({ account_id: process.env.GEMINI_ACCOUNT_ID });
+const gemini_account_credentials = 
+    new AccountCredentials({ 
+        account_info: gemini_account_info,
+        api_key: process.env.GEMINI_API_KEY,
+        secret_key: process.env.GEMINI_SECRET_KEY,
+        passphrase: process.env.GEMINI_PASSPHRASE 
+    })
+
+const coinbase_prime_account_info = new AccountInfo({ account_id: process.env.COINBASE_PRIME_ACCOUNT_ID })
+const coinbase_prime_account_credentials = 
+    new AccountCredentials({
+        account_info: coinbase_prime_account_info,
+        api_key: process.env.COINBASE_PRIME_API_KEY,
+        secret_key: process.env.COINBASE_PRIME_SECRET_KEY,
+        passphrase: process.env.COINBASE_PRIME_PASSPHRASE 
+    })
+
+const account_credentials_list = [
+    gemini_account_credentials,
+    coinbase_prime_account_credentials
+]
+
 const client = 
     new Client({
-        account_id: account_id,
         client_id: parseInt(process.env.CLIENT_ID),
         sender_comp_id: String(uuidv4()),
-        api_key: process.env.API_KEY,
-        secret_key: process.env.SECRET_KEY,
-        passphrase: process.env.PASSPHRASE,
+        account_credentials_list,
         curve_server_key: process.env.CURVE_SERVER_KEY,
         tes_socket_endpoint: process.env.TCP_ADDRESS,
         backend_socket_endpoint: process.env.INPROC_ADDRESS
     });
 
 client.sendLogonMessage(response => console.log(response));
-setTimeout(() => client.sendGetAccountBalancesMessage(response => console.log(response)), 10000);
+setTimeout(
+    () => 
+        client.sendGetAccountBalancesMessage({
+            account_id: process.env.COINBASE_PRIME_ACCOUNT_ID,
+            onResponse: response => console.log(response)
+        }), 10000);
 setTimeout(() => client.sendLogoffMessage(response => console.log(response)), 20000);
-const accountInfo = new AccountInfo({account_id});
-setTimeout(() => client.sendPlaceOrderMessage({placeOrderArguments: new PlaceOrderArguments({accountInfo: accountInfo,
-    clientOrderId: 'client_order_id', symbol: 'BTC/USD', side: 'buy',
-    quantity: 5.0, price: 10.0})}), 5000);
-setTimeout(() => client.sendGetOrderStatusMessage({accountInfo: accountInfo,
-    orderId: 'cb146384-c577-4d72-b29e-01b74549939f'}), 5000);
+// const accountInfo = new AccountInfo({account_id});
+// setTimeout(() => client.sendPlaceOrderMessage({placeOrderArguments: new PlaceOrderArguments({accountInfo: accountInfo,
+//     clientOrderId: 'client_order_id', symbol: 'BTC/USD', side: 'buy',
+//     quantity: 5.0, price: 10.0})}), 5000);
+// setTimeout(() => client.sendGetOrderStatusMessage({accountInfo: accountInfo,
+//     orderId: 'cb146384-c577-4d72-b29e-01b74549939f'}), 5000);
