ec7fc0f6e45d39f311830de6f440a2ed122950b1
Liam Duffy <liamdanielduffy@gmail.com>
2019-01-08 18:49:25 -0500

    replaces message parsing methods in message responder with MessageParser class methods

--- src/tes_client/messages/MessageResponder.js
+++ src/tes_client/messages/MessageResponder.js
@@ -1,8 +1,6 @@
 import { Observable } from "rxjs";
-import { message_body_types } from '~/tes_client/constants'
 
-const capnp = require("capnp");
-const msgs_capnp = require("~/CommunicationProtocol/TradeMessage.capnp");
+import MessageParser from './MessageParser'
 
 class MessageResponder {
 
@@ -15,7 +13,7 @@ class MessageResponder {
 		this.message_observer = Observable.create((observer) => {
 			this.tes_socket.setOnMessage({
 				onMessage: message => {
-					const { message_body_type, parsed_message_body_contents } = this.parseMessage({ message });
+					const { message_body_type, parsed_message_body_contents } = MessageParser.parseMessage({ message });
 					observer.next({
 						message_body_type,
 						parsed_message_body_contents
@@ -34,118 +32,6 @@ class MessageResponder {
 			callback(parsed_message_body_contents) 
 		})
 	}
-
-	parseMessage = ({ message }) => {
-		const { message_body_type, message_body_contents } = this.parseBinaryMessageBody({ binary_message: message });
-		const parsed_message_body_contents = this.parseMessageBodyContents({
-			message_body_type,
-			message_body_contents
-		});
-		return { message_body_type, parsed_message_body_contents }
-	}
-
-	parseBinaryMessageBody = ({ binary_message }) => {
-		const message = capnp.parse(msgs_capnp.TradeMessage, binary_message);
-		const message_body = message.type.response.body;
-		const message_body_type = Object.keys(message_body)[0];
-		const message_body_contents = message_body[message_body_type];
-		return { message_body_type, message_body_contents };
-	};
-
-	parseMessageBodyContents = ({ message_body_type, message_body_contents }) => {
-		switch (message_body_type) {
-			case message_body_types.LOGON_COMPLETE:
-				return this.parseLogonComplete({ message_body_contents });
-
-			case message_body_types.ACCOUNT_BALANCES_REPORT:
-				return this.parseAccountBalancesReport({ message_body_contents });
-
-			case message_body_types.LOGOFF_COMPLETE:
-				return this.parseLogoffComplete({ message_body_contents });
-
-			case message_body_types.ACCOUNT_DATA_REPORT:
-				return this.parseAccountDataReport({ message_body_contents });
-
-			case message_body_types.EXECUTION_REPORT:
-				return this.parseExecutionReport({ message_body_contents });
-
-			default:
-				return { message_body_type };
-		}
-	};
-
-	parseAccountBalancesReport = ({ message_body_contents }) => {
-		const { accountInfo } = message_body_contents;
-		const account_info = {
-			account_id: accountInfo.accountID,
-			exchange: accountInfo.exchange,
-			account_type: accountInfo.accountType,
-			exchange_account_id: accountInfo.exchangeAccountID,
-			exchange_client_id: accountInfo.exchangeClientID
-		}
-		const balances = message_body_contents.balances.map(balance => ({
-			currency: balance.currency,
-			full_balance: balance.fullBalance,
-			available_balance: balance.availableBalance
-		}));
-		return { account_info, balances };
-	};
-
-	parseLogonComplete = ({ message_body_contents }) => {
-		const { success, message, clientAccounts } = message_body_contents;
-		return { success, message, client_accounts: clientAccounts };
-	};
-
-	parseAccountDataReport = ({ message_body_contents }) => {
-		const { accountInfo, balances, openPositions, orders } = message_body_contents;
-		return { account_info: accountInfo, balances, openPositions, orders };
-	};
-
-	parseLogoffComplete = ({ message_body_contents }) => {
-		const { success, message } = message_body_contents;
-		return { success, message };
-	};
-
-	parseExecutionReport = ({ message_body_contents }) => {
-		const {
-			orderID,
-			clientOrderID,
-			clientOrderLinkID,
-			exchangeOrderID,
-			accountInfo,
-			symbol,
-			side,
-			orderType,
-			quantity,
-			price,
-			timeInForce,
-			leverageType,
-			leverage,
-			orderStatus,
-			filledQuantity,
-			avgFillPrice,
-			rejectionReason
-		} = message_body_contents;
-		return {
-			orderID,
-			clientOrderID,
-			clientOrderLinkID,
-			exchangeOrderID,
-			accountInfo,
-			symbol,
-			side,
-			orderType,
-			quantity,
-			price,
-			timeInForce,
-			leverageType,
-			leverage,
-			orderStatus,
-			filledQuantity,
-			avgFillPrice,
-			rejectionReason
-		};
-	};
 }
 
 export default MessageResponder;
