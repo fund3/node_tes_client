e5e527c41b2d9a539f4747eadbfcebba7ded953b
Calvin Leung <calvinleungyk@gmail.com>
2019-03-11 22:19:34 +0800

    Updated MessageBodyFactory spacings to spaces instead of tabs

--- src/tesClient/factories/MessageBodyFactory.js
+++ src/tesClient/factories/MessageBodyFactory.js
@@ -3,132 +3,132 @@ import isNil from "lodash/isNil";
 import { messageBodyTypes } from "~/tesClient/constants";
 
 class MessageBodyFactory {
-	static buildMessageBody = ({ messageBodyType, messageBodyContents }) => {
-		switch (messageBodyType) {
-			case messageBodyTypes.HEARTBEAT:
-				return { [messageBodyTypes.HEARTBEAT]: "" };
+    static buildMessageBody = ({ messageBodyType, messageBodyContents }) => {
+        switch (messageBodyType) {
+            case messageBodyTypes.HEARTBEAT:
+                return { [messageBodyTypes.HEARTBEAT]: "" };
 
-			case messageBodyTypes.TEST:
-				return { [messageBodyTypes.TEST]: messageBodyContents };
+            case messageBodyTypes.TEST:
+                return { [messageBodyTypes.TEST]: messageBodyContents };
 
-			case messageBodyTypes.GET_SERVER_TIME:
-				return { [messageBodyTypes.GET_SERVER_TIME]:
+            case messageBodyTypes.GET_SERVER_TIME:
+                return { [messageBodyTypes.GET_SERVER_TIME]:
                     messageBodyContents};
 
             case messageBodyTypes.LOGON:
-				return { [messageBodyTypes.LOGON]: messageBodyContents};
+                return { [messageBodyTypes.LOGON]: messageBodyContents};
 
-			case messageBodyTypes.LOGOFF:
-				return { [messageBodyTypes.LOGOFF]: "" };
+            case messageBodyTypes.LOGOFF:
+                return { [messageBodyTypes.LOGOFF]: "" };
 
             case messageBodyTypes.AUTHORIZATION_REFRESH:
-				return { [messageBodyTypes.AUTHORIZATION_REFRESH]:
+                return { [messageBodyTypes.AUTHORIZATION_REFRESH]:
                     messageBodyContents};
 
-			case messageBodyTypes.PLACE_SINGLE_ORDER:
-				return { [messageBodyTypes.PLACE_SINGLE_ORDER]:
+            case messageBodyTypes.PLACE_SINGLE_ORDER:
+                return { [messageBodyTypes.PLACE_SINGLE_ORDER]:
                     messageBodyContents };
 
-			case messageBodyTypes.REPLACE_ORDER:
-				return { [messageBodyTypes.REPLACE_ORDER]:
-					messageBodyContents };
+            case messageBodyTypes.REPLACE_ORDER:
+                return { [messageBodyTypes.REPLACE_ORDER]:
+                    messageBodyContents };
 
-			case messageBodyTypes.CANCEL_ORDER:
-				return { [messageBodyTypes.CANCEL_ORDER]:
-					messageBodyContents };
+            case messageBodyTypes.CANCEL_ORDER:
+                return { [messageBodyTypes.CANCEL_ORDER]:
+                    messageBodyContents };
 
-			case messageBodyTypes.GET_ORDER_STATUS:
-				return { [messageBodyTypes.GET_ORDER_STATUS]:
-					messageBodyContents };
+            case messageBodyTypes.GET_ORDER_STATUS:
+                return { [messageBodyTypes.GET_ORDER_STATUS]:
+                    messageBodyContents };
 
-			case messageBodyTypes.GET_ACCOUNT_DATA:
-				return { [messageBodyTypes.GET_ACCOUNT_DATA]:
-					messageBodyContents };
+            case messageBodyTypes.GET_ACCOUNT_DATA:
+                return { [messageBodyTypes.GET_ACCOUNT_DATA]:
+                    messageBodyContents };
 
-			case messageBodyTypes.GET_ACCOUNT_BALANCES:
-				return { [messageBodyTypes.GET_ACCOUNT_BALANCES]:
-					messageBodyContents };
+            case messageBodyTypes.GET_ACCOUNT_BALANCES:
+                return { [messageBodyTypes.GET_ACCOUNT_BALANCES]:
+                    messageBodyContents };
 
-			case messageBodyTypes.GET_OPEN_POSITIONS:
-				return { [messageBodyTypes.GET_OPEN_POSITIONS]:
-					messageBodyContents };
+            case messageBodyTypes.GET_OPEN_POSITIONS:
+                return { [messageBodyTypes.GET_OPEN_POSITIONS]:
+                    messageBodyContents };
 
-			case messageBodyTypes.GET_WORKING_ORDERS:
-				return { [messageBodyTypes.GET_WORKING_ORDERS]:
-					messageBodyContents };
+            case messageBodyTypes.GET_WORKING_ORDERS:
+                return { [messageBodyTypes.GET_WORKING_ORDERS]:
+                    messageBodyContents };
 
-			case messageBodyTypes.GET_COMPLETED_ORDERS:
-				return { [messageBodyTypes.GET_COMPLETED_ORDERS]:
-					messageBodyContents };
+            case messageBodyTypes.GET_COMPLETED_ORDERS:
+                return { [messageBodyTypes.GET_COMPLETED_ORDERS]:
+                    messageBodyContents };
 
-			case messageBodyTypes.GET_EXCHANGE_PROPERTIES:
-				return { [messageBodyTypes.GET_EXCHANGE_PROPERTIES]:
-					messageBodyContents };
+            case messageBodyTypes.GET_EXCHANGE_PROPERTIES:
+                return { [messageBodyTypes.GET_EXCHANGE_PROPERTIES]:
+                    messageBodyContents };
 
-			default:
-				return {};
-		}
-	};
+            default:
+                return {};
+        }
+    };
 
-	static buildHeartbeatMessageBody = () => {
-		const messageBodyType = messageBodyTypes.HEARTBEAT;
-		return MessageBodyFactory.buildMessageBody({ messageBodyType });
-	};
+    static buildHeartbeatMessageBody = () => {
+        const messageBodyType = messageBodyTypes.HEARTBEAT;
+        return MessageBodyFactory.buildMessageBody({ messageBodyType });
+    };
 
-	static buildTestMessageBody = ({ testMessageParams }) => {
-		const messageBodyType = messageBodyTypes.TEST;
-		return MessageBodyFactory.buildMessageBody({
+    static buildTestMessageBody = ({ testMessageParams }) => {
+        const messageBodyType = messageBodyTypes.TEST;
+        return MessageBodyFactory.buildMessageBody({
             messageBodyType, messageBodyContents: testMessageParams });
-	};
+    };
 
-	static buildGetServerTimeMessageBody = () => {
-		const messageBodyType = messageBodyTypes.GET_SERVER_TIME;
-		return MessageBodyFactory.buildMessageBody({ messageBodyType });
-	};
+    static buildGetServerTimeMessageBody = () => {
+        const messageBodyType = messageBodyTypes.GET_SERVER_TIME;
+        return MessageBodyFactory.buildMessageBody({ messageBodyType });
+    };
 
-	static buildLogonMessageBody = ({ logonParams }) => {
-		    const messageBodyType = messageBodyTypes.LOGON;
-		    return MessageBodyFactory.buildMessageBody({
+    static buildLogonMessageBody = ({ logonParams }) => {
+            const messageBodyType = messageBodyTypes.LOGON;
+            return MessageBodyFactory.buildMessageBody({
                 messageBodyType,
                 messageBodyContents: logonParams
-		    });
-	};
+            });
+    };
 
-	static buildLogoffMessageBody = () => {
-		const messageBodyType = messageBodyTypes.LOGOFF;
-		return MessageBodyFactory.buildMessageBody({ messageBodyType });
-	};
+    static buildLogoffMessageBody = () => {
+        const messageBodyType = messageBodyTypes.LOGOFF;
+        return MessageBodyFactory.buildMessageBody({ messageBodyType });
+    };
 
-	static buildPlaceSingleOrderMessageBody = ({
-		placeOrderParams
-	 }) => {
-		const messageBodyType = messageBodyTypes.PLACE_SINGLE_ORDER;
-		return MessageBodyFactory.buildMessageBody({
-			messageBodyType,
-			messageBodyContents: placeOrderParams
-		});
-	};
+    static buildPlaceSingleOrderMessageBody = ({
+        placeOrderParams
+     }) => {
+        const messageBodyType = messageBodyTypes.PLACE_SINGLE_ORDER;
+        return MessageBodyFactory.buildMessageBody({
+            messageBodyType,
+            messageBodyContents: placeOrderParams
+        });
+    };
 
-	static buildReplaceOrderMessageBody = ({
-		replaceOrderParams
-	 }) => {
-		const messageBodyType = messageBodyTypes.REPLACE_ORDER;
-		return MessageBodyFactory.buildMessageBody({
-			messageBodyType,
-			messageBodyContents: replaceOrderParams
-		});
-	};
+    static buildReplaceOrderMessageBody = ({
+        replaceOrderParams
+     }) => {
+        const messageBodyType = messageBodyTypes.REPLACE_ORDER;
+        return MessageBodyFactory.buildMessageBody({
+            messageBodyType,
+            messageBodyContents: replaceOrderParams
+        });
+    };
 
-	static buildCancelOrderMessageBody = ({ cancelOrderParams }) => {
-		const messageBodyType = messageBodyTypes.CANCEL_ORDER;
+    static buildCancelOrderMessageBody = ({ cancelOrderParams }) => {
+        const messageBodyType = messageBodyTypes.CANCEL_ORDER;
 
-		return MessageBodyFactory.buildMessageBody({
-			messageBodyType,
-			messageBodyContents: cancelOrderParams
-		});
-	};
+        return MessageBodyFactory.buildMessageBody({
+            messageBodyType,
+            messageBodyContents: cancelOrderParams
+        });
+    };
 
-	static buildGetOrderStatusMessageBody = ({ getOrderStatusParams }) => {
+    static buildGetOrderStatusMessageBody = ({ getOrderStatusParams }) => {
         const messageBodyType = messageBodyTypes.GET_ORDER_STATUS;
         return MessageBodyFactory.buildMessageBody({
             messageBodyType,
@@ -136,60 +136,60 @@ class MessageBodyFactory {
         });
     };
 
-	static buildGetAccountDataMessageBody = ({ getAccountDataParams }) => {
-		const messageBodyType = messageBodyTypes.GET_ACCOUNT_DATA;
-		return MessageBodyFactory.buildMessageBody({
-			messageBodyType,
-			messageBodyContents: getAccountDataParams
-		});
-	};
+    static buildGetAccountDataMessageBody = ({ getAccountDataParams }) => {
+        const messageBodyType = messageBodyTypes.GET_ACCOUNT_DATA;
+        return MessageBodyFactory.buildMessageBody({
+            messageBodyType,
+            messageBodyContents: getAccountDataParams
+        });
+    };
 
-	static buildGetAccountBalancesMessageBody = ({
+    static buildGetAccountBalancesMessageBody = ({
         getAccountBalancesParams
-	}) => {
-		const messageBodyType = messageBodyTypes.GET_ACCOUNT_BALANCES;
-		return MessageBodyFactory.buildMessageBody({
-			messageBodyType,
-			messageBodyContents: getAccountBalancesParams
-		});
-	};
+    }) => {
+        const messageBodyType = messageBodyTypes.GET_ACCOUNT_BALANCES;
+        return MessageBodyFactory.buildMessageBody({
+            messageBodyType,
+            messageBodyContents: getAccountBalancesParams
+        });
+    };
 
-	static buildGetOpenPositionsMessageBody = ({ getOpenPositionsParams }) => {
-		const messageBodyType = messageBodyTypes.GET_OPEN_POSITIONS;
-		return MessageBodyFactory.buildMessageBody({
-			messageBodyType,
-			messageBodyContents: getOpenPositionsParams
-		});
-	};
+    static buildGetOpenPositionsMessageBody = ({ getOpenPositionsParams }) => {
+        const messageBodyType = messageBodyTypes.GET_OPEN_POSITIONS;
+        return MessageBodyFactory.buildMessageBody({
+            messageBodyType,
+            messageBodyContents: getOpenPositionsParams
+        });
+    };
 
-	static buildGetWorkingOrdersMessageBody = ({ getWorkingOrderParams }) => {
-		const messageBodyType = messageBodyTypes.GET_WORKING_ORDERS;
-		return MessageBodyFactory.buildMessageBody({
-			messageBodyType,
-			messageBodyContents: getWorkingOrderParams
-		});
-	};
+    static buildGetWorkingOrdersMessageBody = ({ getWorkingOrderParams }) => {
+        const messageBodyType = messageBodyTypes.GET_WORKING_ORDERS;
+        return MessageBodyFactory.buildMessageBody({
+            messageBodyType,
+            messageBodyContents: getWorkingOrderParams
+        });
+    };
 
-	static buildGetCompletedOrdersMessageBody = ({
+    static buildGetCompletedOrdersMessageBody = ({
         getCompletedOrdersParams
-	}) => {
+    }) => {
         const messageBodyType = messageBodyTypes.GET_COMPLETED_ORDERS;
 
         return MessageBodyFactory.buildMessageBody({
             messageBodyType,
             messageBodyContents: getCompletedOrdersParams
         });
-	};
+    };
 
-	static buildGetExchangePropertiesMessageBody = ({
+    static buildGetExchangePropertiesMessageBody = ({
         getExchangePropertiesParams
-	}) => {
-		const messageBodyType = messageBodyTypes.GET_EXCHANGE_PROPERTIES;
-		return MessageBodyFactory.buildMessageBody({
-			messageBodyType,
-			messageBodyContents: getExchangePropertiesParams
-		});
-	}
+    }) => {
+        const messageBodyType = messageBodyTypes.GET_EXCHANGE_PROPERTIES;
+        return MessageBodyFactory.buildMessageBody({
+            messageBodyType,
+            messageBodyContents: getExchangePropertiesParams
+        });
+    }
 }
 
 export default MessageBodyFactory;
