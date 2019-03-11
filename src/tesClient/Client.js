3dffa560a26423ae68710a80f271bb619c7bb252
Calvin Leung <calvinleungyk@gmail.com>
2019-03-11 22:10:09 +0800

    Updated client

--- src/tesClient/Client.js
+++ src/tesClient/Client.js
@@ -9,16 +9,21 @@ class Client {
 	constructor({
 		clientId,
 		senderCompId,
+        accessToken = undefined,
 		accountCredentialsList,
 		curveServerKey,
 		tesSocketEndpoint,
 		backendSocketEndpoint
 	}) {
-		this.messageFactory = new MessageFactory({
-			clientId,
-			senderCompId,
-			accountCredentialsList
-		});
+	    this.clientId = clientId;
+		this.senderCompId = senderCompId;
+		this.accountCredentialsList = accountCredentialsList;
+		this.accessToken = accessToken;
+        this.defaultRequestHeader = new RequestHeader({
+            clientId, senderCompId, accessToken
+        });
+
+		this.messageFactory = new MessageFactory();
 
 		this.messenger = new Messenger({
 			curveServerKey,
@@ -27,74 +32,94 @@ class Client {
 		});
 	}
 
-	sendHeartbeatMessage = ({ onResponse, requestHeader }) => {
-		const heartbeatMessage = this.messageFactory.buildHeartbeatMessage({
-			requestHeader
-		});
-		this.messenger.sendMessage({
-			responseMessageBodyType: messageBodyTypes.HEARTBEAT,
-			message: heartbeatMessage,
-			onResponse
-		});
+	updateAccessToken = ({ newAccessToken }) => {
+		this.accessToken = newAccessToken;
+		this.defaultRequestHeader.accessToken = newAccessToken;
 	};
 
-	sendLogonMessage = ({ onResponse }) => {
-		const logonMessage = this.messageFactory.buildLogonMessage();
-		this.messenger.sendMessage({
-			responseMessageBodyType: messageBodyTypes.LOGON_COMPLETE,
-			message: logonMessage,
-			onResponse
-		});
+	sendHeartbeatMessage = ({
+        requestHeader = this.defaultRequestHeader,
+        onResponse
+	}) => {
+        const heartbeatMessage =
+            this.messageFactory.buildHeartbeatMessage({
+                requestHeader
+            });
+        this.messenger.sendMessage({
+            responseMessageBodyType: messageBodyTypes.HEARTBEAT,
+            message: heartbeatMessage,
+            onResponse
+        });
 	};
 
-	sendGetAccountBalancesMessage = ({ accountId, onResponse }) => {
-		const getAccountBalancesMessage =
-			this.messageFactory.buildGetAccountBalancesMessage({
-			    accountId
-			});
-		this.messenger.sendMessage({
-			responseMessageBodyType: messageBodyTypes.ACCOUNT_BALANCES_REPORT,
-			message: getAccountBalancesMessage,
-			onResponse
-		});
+	sendTestMessage = ({
+        requestHeader = this.defaultRequestHeader,
+        testMessageParams,
+        onResponse
+	}) => {
+        const testMessage =
+            this.messageFactory.buildTestMessage({
+                requestHeader,
+                testMessageParams
+            });
+        this.messenger.sendMessage({
+            responseMessageBodyType: messageBodyTypes.TEST,
+            message: testMessage,
+            onResponse
+        });
 	};
 
-	sendLogoffMessage = ({ onResponse }) => {
-		const logoffMessage = this.messageFactory.buildLogoffMessage();
+	sendGetServerTimeMessage = ({
+        requestHeader = this.defaultRequestHeader,
+        onResponse
+	}) => {
+        const getServerTimeMessage =
+            this.messageFactory.buildGetServerTimeMessage({
+                requestHeader
+            });
+        this.messenger.sendMessage({
+            responseMessageBodyType: messageBodyTypes.SERVER_TIME,
+            message: getServerTimeMessage,
+            onResponse
+        });
+	};
+
+	sendLogonMessage = ({
+        requestHeader = this.defaultRequestHeader,
+        logonParams,
+        onResponse
+	}) => {
+        const logonMessage = this.messageFactory.buildLogonMessage({
+            requestHeader, logonParams });
+        this.messenger.sendMessage({
+            responseMessageBodyType: messageBodyTypes.LOGON_ACK,
+            message: logonMessage,
+            onResponse
+        });
+	};
+
+	sendLogoffMessage = ({
+        requestHeader = this.defaultRequestHeader,
+        onResponse
+	}) => {
+		const logoffMessage = this.messageFactory.buildLogoffMessage({
+            requestHeader
+        });
 		this.messenger.sendMessage({
-			responseMessageBodyType: messageBodyTypes.LOGOFF_COMPLETE,
+			responseMessageBodyType: messageBodyTypes.LOGOFF_ACK,
 			message: logoffMessage,
 			onResponse
 		});
 	};
 
-	sendPlaceOrderMessage = ({
-		onResponse,
-		accountInfo,
-		clientOrderId,
-		symbol,
-		side,
-		quantity,
-		orderType,
-		price,
-		timeInForce,
-		leverageType,
-		leverage,
-		clientOrderLinkId
+	sendPlaceSingleOrderMessage = ({
+		requestHeader = this.defaultRequestHeader,
+        placeOrderParams,
+        onResponse
 	}) => {
-		const placeOrderMessage = this.messageFactory.buildPlaceOrderMessage({
-			accountInfo,
-			clientOrderId,
-			symbol,
-			side,
-			quantity,
-			orderType,
-			price,
-			timeInForce,
-			leverageType,
-			leverage,
-			clientOrderLinkId
-		});
+		const placeOrderMessage =
+            this.messageFactory.buildPlaceSingleOrderMessage({
+                requestHeader, placeOrderParams });
 
 		this.messenger.sendMessage({
 			responseMessageBodyType: messageBodyTypes.EXECUTION_REPORT,
@@ -103,43 +128,31 @@ class Client {
 		});
 	};
 
-	sendGetOrderStatusMessage = ({ onResponse, accountInfo, orderId }) => {
-		const getOrderStatusMessage =
-			this.messageFactory.buildGetOrderStatusMessage({
-				accountInfo,
-				orderId
-			});
+	sendReplaceOrderMessage = ({
+		requestHeader = this.defaultRequestHeader,
+        replaceOrderParams,
+        onResponse
+	}) => {
+		const replaceOrderMessage =
+            this.messageFactory.buildReplaceOrderMessage({
+                requestHeader, replaceOrderParams });
+
 		this.messenger.sendMessage({
 			responseMessageBodyType: messageBodyTypes.EXECUTION_REPORT,
-			message: getOrderStatusMessage,
+			message: replaceOrderMessage,
 			onResponse
 		});
 	};
 
-	sendGetAccountDataMessage = ({ onResponse, accountInfo }) => {
-		const getAccountDataMessage =
-			this.messageFactory.buildGetAccountDataMessage({accountInfo});
-		this.messenger.sendMessage({
-			responseMessageBodyType: messageBodyTypes.ACCOUNT_DATA_REPORT,
-			message: getAccountDataMessage,
-			onResponse: ({ accountInfo, balances, openPositions, orders }) =>
-				console.log(accountInfo, balances, openPositions, orders)
-		});
-	};
-
-	sendGetWorkingOrdersMessage = ({ onResponse, accountId }) => {
-		const getWorkingOrdersMessage =
-			this.messageFactory.buildGetWorkingOrdersMessage({accountId});
-		this.messenger.sendMessage({
-			responseMessageBodyType: messageBodyTypes.WORKING_ORDERS_REPORT,
-			message: getWorkingOrdersMessage,
-			onResponse
-		});
-	};
-
-	sendCancelOrderMessage = ({ onResponse, accountId, orderId }) => {
+	sendCancelOrderMessage = ({
+        requestHeader = this.defaultRequestHeader,
+        cancelOrderParams,
+        onResponse
+	}) => {
 		const cancelOrderMessage =
-			this.messageFactory.buildCancelOrderMessage({accountId, orderId});
+			this.messageFactory.buildCancelOrderMessage({
+                requestHeader, cancelOrderParams});
+
 		this.messenger.sendMessage({
 			responseMessageBodyType: messageBodyTypes.EXECUTION_REPORT,
 			message: cancelOrderMessage,
@@ -147,11 +160,90 @@ class Client {
 		});
 	};
 
-	sendGetCompletedOrdersMessage = (
-		{ onResponse, accountId, count, since}) => {
+	sendGetOrderStatusMessage = ({
+        requestHeader = this.defaultRequestHeader,
+        getOrderStatusParams,
+        onResponse
+	}) => {
+		const getOrderStatusMessage =
+			this.messageFactory.buildGetOrderStatusMessage({
+                requestHeader, getOrderStatusParams });
+		this.messenger.sendMessage({
+			responseMessageBodyType: messageBodyTypes.EXECUTION_REPORT,
+			message: getOrderStatusMessage,
+			onResponse
+		});
+	};
+
+	sendGetAccountDataMessage = ({
+        requestHeader = this.defaultRequestHeader,
+        getAccountDataParams,
+        onResponse
+	}) => {
+		const getAccountDataMessage =
+			this.messageFactory.buildGetAccountDataMessage({
+                requestHeader, getAccountDataParams });
+		this.messenger.sendMessage({
+			responseMessageBodyType: messageBodyTypes.ACCOUNT_DATA_REPORT,
+			message: getAccountDataMessage,
+			onResponse
+		});
+	};
+
+	sendGetAccountBalancesMessage = ({
+        requestHeader = this.defaultRequestHeader,
+        getAccountBalancesParams,
+        onResponse
+	}) => {
+        const getAccountBalancesMessage =
+            this.messageFactory.buildGetAccountBalancesMessage({
+                requestHeader, getAccountBalancesParams});
+        this.messenger.sendMessage({
+            responseMessageBodyType:
+                messageBodyTypes.ACCOUNT_BALANCES_REPORT,
+            message: getAccountBalancesMessage,
+            onResponse
+        });
+	};
+
+	sendGetOpenPositionsMessage = ({
+        requestHeader = this.defaultRequestHeader,
+        getOpenPositionsParams,
+        onResponse
+	}) => {
+		const getOpenPositionsMessage =
+			this.messageFactory.buildGetOpenPositionsMessage({
+                requestHeader, getOpenPositionsParams });
+		this.messenger.sendMessage({
+			responseMessageBodyType: messageBodyTypes.OPEN_POSITIONS_REPORT,
+			message: getOpenPositionsMessage,
+			onResponse
+		});
+	};
+
+	sendGetWorkingOrdersMessage = ({
+        requestHeader = this.defaultRequestHeader,
+        getWorkingOrderParams,
+        onResponse
+	}) => {
+		const getWorkingOrdersMessage =
+			this.messageFactory.buildGetWorkingOrdersMessage({
+                requestHeader, getWorkingOrderParams });
+		this.messenger.sendMessage({
+			responseMessageBodyType: messageBodyTypes.WORKING_ORDERS_REPORT,
+			message: getWorkingOrdersMessage,
+			onResponse
+		});
+	};
+
+	sendGetCompletedOrdersMessage = ({
+        requestHeader = this.defaultRequestHeader,
+        getCompletedOrdersParams,
+        onResponse
+	}) => {
 			const getCompletedOrdersMessage =
-				this.messageFactory.buildGetCompletedOrdersMessage(
-					{accountId, count, since});
+				this.messageFactory.buildGetCompletedOrdersMessage({
+                    requestHeader, getCompletedOrdersParams });
 			this.messenger.sendMessage({
 				responseMessageBodyType:
 					messageBodyTypes.COMPLETED_ORDERS_REPORT,
@@ -160,9 +252,16 @@ class Client {
 			});
 	};
 
-	sendGetExchangePropertiesMessage = ({ onResponse, exchange }) => {
+	sendGetExchangePropertiesMessage = ({
+        requestHeader = this.defaultRequestHeader,
+        getExchangePropertiesParams,
+        onResponse
+	}) => {
 		const getExchangePropertiesMessage =
-			this.messageFactory.buildGetExchangePropertiesMessage({exchange});
+			this.messageFactory.buildGetExchangePropertiesMessage({
+                requestHeader,
+                getExchangePropertiesParams
+			});
 		this.messenger.sendMessage({
 			responseMessageBodyType:
 				messageBodyTypes.EXCHANGE_PROPERTIES_REPORT,
