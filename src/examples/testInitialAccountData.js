2be66267cce4b96e0f78fb4e91b56dbe6058bca2
Calvin Leung <calvinleungyk@gmail.com>
2019-04-12 18:50:12 +0800

    Added account data checks

---
+++ src/examples/testInitialAccountData.js
@@ -0,0 +1,63 @@
+//index.js
+require("dotenv").config();
+
+require("@babel/polyfill");
+import uuidv4 from 'uuid/v4'
+
+import AccountCredentials from '~/tesClient/account/AccountCredentials'
+import Client from '~/tesClient/Client'
+import LogonParams from '~/tesClient/requestParams/LogonParams'
+
+const geminiAccountCredentials =
+    new AccountCredentials({
+        accountId: process.env.GEMINI_ACCOUNT_ID,
+        apiKey: process.env.GEMINI_API_KEY,
+        secretKey: process.env.GEMINI_SECRET_KEY,
+        passphrase: process.env.GEMINI_PASSPHRASE
+    });
+
+const coinbasePrimeAccountCredentials = new AccountCredentials({
+	accountId: process.env.COINBASE_PRIME_ACCOUNT_ID,
+	apiKey: process.env.COINBASE_PRIME_API_KEY,
+	secretKey: process.env.COINBASE_PRIME_SECRET_KEY,
+	passphrase: process.env.COINBASE_PRIME_PASSPHRASE
+});
+
+const accountCredentialsList = [
+    geminiAccountCredentials,
+    coinbasePrimeAccountCredentials
+];
+
+const client =
+    new Client({
+        clientId: parseInt(process.env.CLIENT_ID),
+        senderCompId: String(uuidv4()),
+        accountCredentialsList,
+        curveServerKey: process.env.CURVE_SERVER_KEY,
+        tesSocketEndpoint: process.env.TCP_ADDRESS
+    });
+
+
+setTimeout(() =>
+    client.sendLogonMessage({
+        logonParams: new LogonParams({
+            clientSecret: process.env.CLIENT_SECRET,
+            credentials: client.accountCredentialsList
+        }),
+        requestIdCallback: logonAck => console.log(logonAck)
+    }),
+    2000
+);
+
+const readyCallback = () => {
+    client.sendLogoffMessage(
+        { requestIdCallback: response => console.log(response) })
+};
+
+const waitForClientToBeReady = async (readyCallback) => {
+    await client.ready().catch((err) => console.log(err));
+    console.log('Client is ready!');
+    readyCallback()
+};
+
+waitForClientToBeReady(readyCallback);
